073d84b9701e158be867ed5287184f94e6d3018a
JD Weeks <jdweeks@g.clemson.edu>
2018-03-09 19:39:58 -0500

    getting started with newsapi

--- server.js
+++ server.js
@@ -3,8 +3,51 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const YAML = require('yamljs');
+const rp = require('request-promise');
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+// load selected sources from filesystem
+const sources = YAML.load('sources.yaml');
+
+// expose internal api method to retrieve sources
+app.get('/api/sources', (req, res) => {
+  res.send({ sources: sources });
+});
+
+// build string of sources for API call
+let sourceStr = '';
+sources.forEach(function(source) {
+  sourceStr += source + ',';
+});
+sourceStr = sourceStr.substring(0, sourceStr.length-1);
+
+// set newsapi key
+const apiKey = '136212b813b1423e88777c8e10c4a80b';
+
+// set newsapi call options
+const apiOpts = {
+  uri: 'https://newsapi.org/v2/top-headlines',
+  qs: {
+    sources: sourceStr,
+    apiKey: apiKey
+  },
+  json: true
+} 
+
+// create function to retrieve top headlines from newsapi
+function getTopHeadlines() {
+  return rp(apiOpts);
+}
+
+// expose internal api method to retrieve top headlines
+app.get('/api/news', (req, res) => {
+  getTopHeadlines()
+    .then((news) => { res.json(news); })
+    .error((err) => { res.status(500).send(err); });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
