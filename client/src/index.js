6cb4086a47db9be12ed1c6d4cdd2d9f20909df71
JD Weeks <jdweeks@g.clemson.edu>
2018-03-15 10:40:13 -0400

    make article links open in new tab

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import $ from 'jquery';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// make all article links open in new tab
+$(document).ready(function() {
+  var website = window.location.hostname;
+  var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
+                                      + website
+                                      + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
+
+  $('a').filter(function() {
+    var href = $(this).attr('href');
+    return !internalLinkRegex.test(href);
+  })
+  .each(function() {
+    $(this).attr('target', '_blank');
+  });
+});
