934ee8d1e11135e1af81aac0875a8f30b157ecc4
JD Weeks <jdweeks@g.clemson.edu>
2018-03-18 15:38:09 -0400

    add logo / favicon

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import logo from './logo.svg';
 import './Menu.css';
+
 
 export default class Menu extends React.Component {
   constructor(props) {
@@ -21,6 +23,7 @@ export default class Menu extends React.Component {
     return (
       <div>
         <Navbar className="customNav" color="dark" dark>
+          <img height="50px" width="50px" src={logo} alt="logo"/>&nbsp;&nbsp;&nbsp;
           <NavbarBrand href="/" className="mr-auto">factual future</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
